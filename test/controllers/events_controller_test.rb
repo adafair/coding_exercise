6486a99b5cc64c01f280ab6f18ba96dff8f485ef
Ada Fairweather <adafairweather@gmail.com>
2017-01-19 14:44:48 -0800

    Further verify events controller responses
    I also added a test for the organization events index call.

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -9,6 +9,39 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get events_url, as: :json
     assert_response :success
+
+    events = JSON.parse response.body
+    assert events.kind_of?(Array)
+    assert(events.length > 0)
+
+    Event.find_each do |db_event|
+      event = events.select{|e| e['id'] == db_event.id}.first
+      assert_not_nil event
+      assert_equal db_event.organization_id, event['organization_id']
+      assert_equal db_event.message, event['message']
+      assert_equal db_event.hostname, event['hostname']
+      assert_equal db_event.created_at.to_json.gsub('"', ''), event['created_at']
+      assert_equal db_event.updated_at.to_json.gsub('"', ''), event['updated_at']
+    end
+  end
+
+  test "should get index for organization" do
+    get events_url(organization_id: @organization.name), as: :json
+    assert_response :success
+
+    events = JSON.parse response.body
+    assert events.kind_of?(Array)
+    assert_equal @organization.events.count, events.length
+
+    @organization.events.find_each do |db_event|
+      event = events.select{|e| e['id'] == db_event.id}.first
+      assert_not_nil event
+      assert_equal db_event.organization_id, event['organization_id']
+      assert_equal db_event.message, event['message']
+      assert_equal db_event.hostname, event['hostname']
+      assert_equal db_event.created_at.to_json.gsub('"', ''), event['created_at']
+      assert_equal db_event.updated_at.to_json.gsub('"', ''), event['updated_at']
+    end
   end
 
   test "should create event" do
@@ -23,16 +56,43 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_response 201
+
+    event = JSON.parse response.body
+    assert_not_nil event
+    assert event['id'].present?
+    assert_equal @organization.id, event['organization_id']
+    assert_equal "New Event", event['message']
+    assert_equal "example-1.org", event['hostname']
+    assert event['created_at'].present?
+    assert event['updated_at'].present?
   end
 
   test "should show event" do
     get event_url(@event), as: :json
     assert_response :success
+
+    event = JSON.parse response.body
+    assert_not_nil event
+    assert_equal @event.id, event['id']
+    assert_equal @event.organization_id, event['organization_id']
+    assert_equal @event.message, event['message']
+    assert_equal @event.hostname, event['hostname']
+    assert_equal @event.created_at.to_json.gsub('"', ''), event['created_at']
+    assert_equal @event.updated_at.to_json.gsub('"', ''), event['updated_at']
   end
 
   test "should update event" do
+    org_id = @event.organization_id
+
     patch event_url(@event), params: { event: { message: "Updated Event", hostname: "example-2.org" } }, as: :json
     assert_response 200
+
+    event = JSON.parse response.body
+    assert_not_nil event
+    assert_equal @event.id, event['id']
+    assert_equal org_id, event['organization_id']
+    assert_equal "Updated Event", event['message']
+    assert_equal "example-2.org", event['hostname']
   end
 
   test "should destroy event" do
